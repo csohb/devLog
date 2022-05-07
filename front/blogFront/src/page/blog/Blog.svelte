7520dcf626db8bd4f486d30f03480548158c1a6c
yeong <rladudwl210@naver.com>
2022-05-07 14:42:33 +0900

    add: [front] router 구조

---
+++ front/blogFront/src/page/blog/Blog.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Footer from "../../components/Footer.svelte";
+import Header from "../../components/Header.svelte";
+
+</script>
+
+
+<Header />
+<main>
+    Blog
+</main>
+<Footer />
---
+++ front/blogFront/src/page/contact/Contact.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Footer from "../../components/Footer.svelte";
+import Header from "../../components/Header.svelte";
+
+</script>
+
+<Header />
+
+<main>
+  Contact 
+</main>
+
+<Footer />

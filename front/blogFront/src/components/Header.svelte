832239532519c709867c77ce02f39d93e7fcd0a4
yeong <rladudwl210@naver.com>
2022-05-14 17:31:34 +0900

    feat: [front] 모바일 버튼 추가

--- front/blogFront/src/components/Header.svelte
+++ front/blogFront/src/components/Header.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
     import {link} from 'svelte-spa-router'
+
+    let toggleEl:HTMLElement
+    let navbarEl:HTMLElement
+
+    let isToggle:boolean = false
+
+    function toggleBtn():void{
+        isToggle = !isToggle
+    
+        if(isToggle){
+            toggleEl.classList.add('on')
+            navbarEl.classList.add('on')
+        }else{
+            toggleEl.classList.remove('on')
+            navbarEl.classList.remove('on')
+        }
+    }
+
 </script>
 
 <header class="header">
@@ -8,10 +26,24 @@
         <a href="/">Devlog</a>
     </h1>
     <nav>
-        <a href="/about" use:link>About</a>
-        <a href="/blog" use:link>Blog</a>
-        <a href="/story" use:link>Story</a>
-        <a href="/contact" use:link>Contact</a>
+        <ul class="navbar-menu" bind:this={navbarEl}>
+            <li>
+                <a href="/about" use:link>About</a>
+            </li>
+            <li>
+                <a href="/blog" use:link>Blog</a>
+            </li>
+            <li>
+                <a href="/story" use:link>Story</a>
+            </li>
+            <li>
+                <a href="/contact" use:link>Contact</a>
+            </li>
+        </ul>
+
+        <button class="navbar-toggleBtn" on:click={toggleBtn} bind:this={toggleEl}>
+            <span></span><span></span><span></span>
+        </button>
     </nav>
 </div>
 </header>

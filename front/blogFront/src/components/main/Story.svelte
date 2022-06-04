53b4f52955a02077d2ab608e8ebf8e8a1fdc2863
yeong <rladudwl210@naver.com>
2022-06-04 13:38:59 +0900

    add: [front] 메인 swiper 작업중

--- front/blogFront/src/components/main/Story.svelte
+++ front/blogFront/src/components/main/Story.svelte
@@ -1,25 +1,76 @@
 <script lang="ts">
-
+  import { Swiper, SwiperSlide } from 'svelte-swiper';
+  
+  const options = {
+    slidesPerView: 1,
+   
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+  }
+  };
 </script>
 
 <div class="inner">
    <div class="main-stroy-wrap">
        <h1>STORY</h1>
-       <div class="profile-wrap">
-           <div class="profile-id">
-                <div class="profile-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
-                <div class="profile-text">
-                    <p>이름 <span>Backend Developer</span></p>
-                    <p>저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.</p>
+       <Swiper {options}>
+        <SwiperSlide> 
+            <div class="main-stroy-item">
+            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
+                <div class="main-story-text">
+                    <p class="main-story-text-label">Backend</p>
+                    <h3>제목</h3>
+                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
                 </div>
-           </div>
-           <div class="profile-id">
-            <div class="profile-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
-                <div class="profile-text">
-                    <p>이름 <span>Backend Developer</span></p>
-                    <p>저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.</p>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide> 
+            <div class="main-stroy-item">
+            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
+                <div class="main-stro-text">
+                    <p>Backend</p>
+                    <h3>제목</h3>
+                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
                 </div>
-           </div>
-       </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide> 
+            <div class="main-stroy-item">
+            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
+                <div class="main-stro-text">
+                    <p>Backend</p>
+                    <h3>제목</h3>
+                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
+                </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide> 
+            <div class="main-stroy-item">
+            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
+                <div class="main-stro-text">
+                    <p>Backend</p>
+                    <h3>제목</h3>
+                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
+                </div>
+            </div>
+        </SwiperSlide>
+      
+ 
+      
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+      </Swiper>
    </div> 
 </div>
--- front/blogFront/src/main.ts
+++ front/blogFront/src/main.ts
@@ -1,4 +1,5 @@
 import App from './App.svelte';
+import 'swiper/swiper-bundle.css'
 
 const app = new App({
 	target: document.body,

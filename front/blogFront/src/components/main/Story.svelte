c9af240f44e5361def2e9a47c28f22deac2cef0a
yeong <rladudwl210@naver.com>
2022-06-17 19:09:05 +0900

    feat: [front] 메인페이지 레이아웃 완료

--- front/blogFront/src/components/main/Story.svelte
+++ front/blogFront/src/components/main/Story.svelte
@@ -2,7 +2,13 @@
   import { Swiper, SwiperSlide } from 'svelte-swiper';
   
   const options = {
+    loop: true,
     slidesPerView: 1,
+    autoplay: {
+      delay: 2500, // 시간 설정          
+      disableOnInteraction: false, // false-스와이프 후 자동 재생      
+    },
+
    
     pagination: {
       el: '.swiper-pagination',
@@ -25,49 +31,18 @@
    <div class="main-stroy-wrap">
        <h1>STORY</h1>
        <Swiper {options}>
+        {#each Array(5) as _ }
         <SwiperSlide> 
             <div class="main-stroy-item">
             <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
                 <div class="main-story-text">
                     <p class="main-story-text-label">Backend</p>
                     <h3>제목</h3>
-                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
+                    <p class="mian-story-text-description">dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
                 </div>
             </div>
         </SwiperSlide>
-        <SwiperSlide> 
-            <div class="main-stroy-item">
-            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
-                <div class="main-stro-text">
-                    <p>Backend</p>
-                    <h3>제목</h3>
-                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide> 
-            <div class="main-stroy-item">
-            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
-                <div class="main-stro-text">
-                    <p>Backend</p>
-                    <h3>제목</h3>
-                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide> 
-            <div class="main-stroy-item">
-            <div class="main-story-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
-                <div class="main-stro-text">
-                    <p>Backend</p>
-                    <h3>제목</h3>
-                    <p>dev blog 를 만들기 시작한 계기???????이런 설명 간단하게 노출</p>
-                </div>
-            </div>
-        </SwiperSlide>
-      
- 
-      
+        {/each}
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-next" slot="button-next"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>

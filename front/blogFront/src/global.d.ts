5f45cf0c35a487694ebff9bc9433fe6c801c652c
yeong <rladudwl210@naver.com>
2022-06-22 18:49:32 +0900

    feat: [front] 다시 설치

--- front/blogFront/src/global.d.ts
+++ front/blogFront/src/global.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="svelte" />
 
-declare module "*.svelte" {
-  export { SvelteComponentDev as default } from "svelte/internal";
+declare module '*.svelte' {
+  export { SvelteComponentDev as default } from 'svelte/internal';
 }

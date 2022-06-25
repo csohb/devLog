2cbf7b7bda8fa6ab90409ff0511f2a70dbb37ded
yeong <rladudwl210@naver.com>
2022-06-25 15:33:17 +0900

    feat: [front] main 1차 완료

--- front/blogFront/src/components/main/About.svelte
+++ front/blogFront/src/components/main/About.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
 import { onMount } from "svelte";
-import { fetchProfileList } from "../../api/main";
+import mainStore from "../../stores/main"
+
 
 
 onMount(()=>{
-    fetchProfileList().then((res)=>{
-        console.log(res)
-    })
+    // fetchProfileList().then((res)=>{
+    //     console.log(res)
+    // })
+    mainStore.getProfiles()
 })
+
+function imgError(){
+    mainStore.imgError()
+    console.log('img error')
+}
+
+$: console.log('data', $mainStore)
 
 </script>
 
@@ -15,20 +24,23 @@ onMount(()=>{
    <div class="main-about-wrap">
        <h1>ABOUT</h1>
        <div class="profile-wrap">
-           <div class="profile-id">
-                <div class="profile-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
+        {#each $mainStore.profiles as item}
+            <div class="profile-id">
+                <div class="profile-img"><img src="{item.image}" alt="{item.nick_name + ' 이미지'}" on:error={imgError}></div>
                 <div class="profile-text">
-                    <p>이름 <span>Backend Developer</span></p>
-                    <p>저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.</p>
+                    <p>{item.nick_name} <span>{item.dev}</span></p>
+                    <p>{item.intro}</p>
                 </div>
-           </div>
-           <div class="profile-id">
+            </div>
+        {/each}
+      
+           <!-- <div class="profile-id">
             <div class="profile-img"><img src="https://ridicorp.com/wp-content/uploads/2022/03/main-rsz-1-940x627.png" alt="프로필 사진"></div>
                 <div class="profile-text">
                     <p>이름 <span>Backend Developer</span></p>
                     <p>저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.</p>
                 </div>
-           </div>
+           </div> -->
        </div>
    </div> 
 </div>
---
+++ front/blogFront/src/stores/types/main.ts
@@ -0,0 +1,11 @@
+export type MainStore = {
+  profiles: Profiles[];
+};
+
+export type Profiles = {
+  dev: string;
+  image: string;
+  intro: string;
+  nick_name: string;
+  user_id: string;
+};

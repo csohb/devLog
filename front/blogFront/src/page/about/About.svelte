4bfbb85a529f07620ce1348f8fac84373fd24ea4
yeong <rladudwl210@naver.com>
2022-07-10 04:39:22 +0900

    add: [front] 소개 페이지 skill 레이아웃 추가

--- front/blogFront/src/page/about/About.svelte
+++ front/blogFront/src/page/about/About.svelte
@@ -2,6 +2,7 @@
 import Career from "../../components/about/Career.svelte";
 import Introduce from "../../components/about/Introduce.svelte";
 import Project from "../../components/about/Project.svelte";
+import Skills from "../../components/about/Skills.svelte";
 import Footer from "../../components/Footer.svelte";
 import Header from "../../components/Header.svelte";
 </script>
@@ -24,7 +25,9 @@ import Header from "../../components/Header.svelte";
           <div class="sub-about-project">
             <Project />
           </div>
-          <div class="sub-about-skills"></div>
+          <div class="sub-about-skills">
+            <Skills />
+          </div>
           <div class="sub-about-keywords"></div>
         </div>
       </div>

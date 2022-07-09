3d089e59130cc36606bd543b9e84480bc09ca69d
yeong <rladudwl210@naver.com>
2022-07-09 15:04:49 +0900

    add: [front] 소개 페이지 레이아웃 작업중

---
+++ front/blogFront/src/components/about/Career.svelte
@@ -0,0 +1,32 @@
+<h2>Career</h2>
+<div class="timeline">
+  <ul>
+    <li>
+      <div class="bullet pink"></div>
+      <div class="time">2021-03-19 ~ <br />2022-06-30</div>
+      <div class="desc">
+        <h3>Plea</h3>
+        <h4>Mobile App</h4>
+      </div>
+    </li>
+    <li>
+      <div class="bullet green"></div>
+      <div class="time">2021-03-19</div>
+      <div class="desc">
+        <h3>Design Stand Up</h3>
+        <h4>Hangouts</h4>
+        <div class="people">
+          <img
+            src="https://s3.amazonaws.com/uifaces/faces/twitter/ashleyford/128.jpg"
+            alt="" />
+          <img
+            src="https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"
+            alt="" />
+          <img
+            src="https://s3.amazonaws.com/uifaces/faces/twitter/mattsince87/128.jpg"
+            alt="" />
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
--- front/blogFront/src/page/about/About.svelte
+++ front/blogFront/src/page/about/About.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import Career from "../../components/about/Career.svelte";
+import Introduce from "../../components/about/Introduce.svelte";
 import Project from "../../components/about/Project.svelte";
 import Footer from "../../components/Footer.svelte";
 import Header from "../../components/Header.svelte";
@@ -15,23 +17,10 @@ import Header from "../../components/Header.svelte";
         </ul>
         <h1>About Me</h1>
         <div class="sub-about-contents">
-          <div class="sub-about-introduce">
-            <pre>안녕하세요, 코딩하는 것이 즐거운 행복한 개발자입니다.
-저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.
-이곳은 저를 소개하는 동시에, 저의 개발 성장 일지를 기록하는 공간입니다. 어떻게 보면 일종의 회고록으로도 볼 수 있겠네요. 😅
-저에게 문의할 내용이 있으면 언제든지 연락주셔도 좋습니다.
-감사합니다 !</pre>
+          <Introduce />
+          <div class="sub-about-career">
+            <Career />
           </div>
-          <div class="sub-about-info">
-            <h2>Info</h2>
-            <ul>
-              <li><strong>Name</strong>김영지 ( yeong )</li>
-              <li><strong>Date of birth</strong>Feb / 26 / 1993</li>
-              <li><strong>Address</strong>경기도 수원시</li>
-              <li><strong>E-mail</strong>kyj210@gmail.com</li>
-            </ul>
-          </div>
-          <div class="sub-about-career"></div>
           <div class="sub-about-project">
             <Project />
           </div>

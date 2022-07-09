a05fbb5fdcd0f3f9508737f04e64e06a6f5ef9e0
yeong <rladudwl210@naver.com>
2022-07-10 04:39:22 +0900

    add: [front] 소개 페이지 skill 레이아웃 추가

---
+++ front/blogFront/src/components/about/Skills.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import ProgressBar from "@okrad/svelte-progressbar";
+let skills = [
+  {
+    title: "Python",
+    series: [
+      {
+        perc: 10,
+        color: "#5AB6DF",
+      },
+    ],
+  },
+  {
+    title: "JavaScript",
+    series: [
+      {
+        perc: 80,
+        color: "#5AB6DF",
+      },
+    ],
+  },
+];
+</script>
+
+<h2>Major Skills</h2>
+<ul class="progress-bar">
+  {#each skills as item}
+    <li class="progress-bar-item">
+      <p>{item.title}</p>
+      <div>
+        <!-- radial -->
+        <ProgressBar
+          series="{item.series}"
+          style="semicircle"
+          thickness="{10}" />
+      </div>
+    </li>
+  {/each}
+</ul>

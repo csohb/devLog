b6f07e637e325ad1c65a55165295d338f54b4a9e
yeong <rladudwl210@naver.com>
2022-07-09 15:04:49 +0900

    add: [front] 소개 페이지 레이아웃 작업중

---
+++ front/blogFront/src/components/about/Introduce.svelte
@@ -0,0 +1,16 @@
+<div class="sub-about-introduce">
+  <pre>안녕하세요, 코딩하는 것이 즐거운 행복한 개발자입니다.
+저는 현재 백엔드 개발을 하고 있으며, 주로 Python 기반 백엔드 프레임워크를 다루고 있습니다.
+이곳은 저를 소개하는 동시에, 저의 개발 성장 일지를 기록하는 공간입니다. 어떻게 보면 일종의 회고록으로도 볼 수 있겠네요. 😅
+저에게 문의할 내용이 있으면 언제든지 연락주셔도 좋습니다.
+감사합니다 !</pre>
+</div>
+<div class="sub-about-info">
+  <h2>Info</h2>
+  <ul>
+    <li><strong>Name</strong>김영지 ( yeong )</li>
+    <li><strong>Date of birth</strong>Feb / 26 / 1993</li>
+    <li><strong>Address</strong>경기도 수원시</li>
+    <li><strong>E-mail</strong>kyj210@gmail.com</li>
+  </ul>
+</div>

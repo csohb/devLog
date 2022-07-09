40f2c72143ed68dac7d397f7ffeb54caf983feb2
yeong <rladudwl210@naver.com>
2022-07-10 08:27:20 +0900

    add: [front] 소개 페이지 레아아웃 완료

---
+++ front/blogFront/src/components/about/Knowledges.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<h2>Knowledges</h2>
+<ul class="keywords-wrap">
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+  <li>JavaScript</li>
+  <li>docker</li>
+  <li>ci/cd</li>
+</ul>

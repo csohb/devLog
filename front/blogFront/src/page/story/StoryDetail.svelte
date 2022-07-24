02517bea9f2fc5141accb6d1e2de54768d1e0535
yeong <rladudwl210@naver.com>
2022-07-24 15:56:42 +0900

    add: [front] 스토리 화면 레이아웃 작업 중

---
+++ front/blogFront/src/page/story/StoryDetail.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+import Footer from "../../components/Footer.svelte";
+import Header from "../../components/Header.svelte";
+import { link } from "svelte-spa-router";
+import { unlike, like } from "../../icon/Icon";
+
+let isLike = false;
+let likeCount = 0;
+
+function onClickLike() {
+  isLike = !isLike;
+  if (isLike) {
+    likeCount += 1;
+  } else {
+    likeCount -= 1;
+  }
+}
+</script>
+
+<Header />
+<main class="sub-page">
+  <section class="sub-story-wrap">
+    <div class="inner">
+      <div class="sub-story">
+        <h1 class="sub-page-title">Story</h1>
+        <div class="sub-story-detail-title">
+          <span> Backend </span>
+          <h2>제목</h2>
+          <p>devblog를 시작한 계기</p>
+          <div class="sub-story-detail-info">
+            <span>2022-07-24</span>
+            <span>|</span>
+            <span>yeong</span>
+          </div>
+          <div class="sub-story-detail-thumb">
+            <img
+              src="https://ridicorp.com/wp-content/uploads/2022/06/sf_1_thumb@0.5x.jpg"
+              alt="" />
+          </div>
+        </div>
+        <div class="sub-story-detail-contents">내용내용 ㄴ용내용</div>
+        <div class="sub-story-detail-action">
+          <div class="sub-story-liked" on:click="{onClickLike}">
+            {#if isLike}
+              {@html like}
+            {:else}
+              {@html unlike}
+            {/if}
+            {likeCount}
+          </div>
+        </div>
+        <div class="sub-story-detail-btn">
+          <a href="/story" use:link>목록으로 돌아가기</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<Footer />
---
+++ front/blogFront/src/page/story/StoryEditor.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+import Footer from "../../components/Footer.svelte";
+import Header from "../../components/Header.svelte";
+import { link } from "svelte-spa-router";
+</script>
+
+<Header />
+<main class="sub-page">
+  <section class="sub-story-wrap">
+    <div class="inner">
+      <div class="sub-story">
+        <h1 class="sub-page-title">Story</h1>
+        <div class="sub-story-detail-title">
+          <span> Backend </span>
+          <h2>제목</h2>
+          <p>devblog를 시작한 계기</p>
+          <div class="sub-story-detail-thumb">
+            <img
+              src="https://ridicorp.com/wp-content/uploads/2022/06/sf_1_thumb@0.5x.jpg"
+              alt="" />
+          </div>
+          <div class="sub-story-detail-info">
+            <span>2022-07-24</span>
+            <span>|</span>
+            <span>yeong</span>
+          </div>
+        </div>
+        <div class="sub-story-detail-contents">수정</div>
+        <div class="sub-story-detail-btn">
+          <a href="/story" use:link>목록으로 돌아가기</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<Footer />

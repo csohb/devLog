7cd226218e52f8f2334d31463d1529053c4666f2
yeong <rladudwl210@naver.com>
2022-07-24 15:56:42 +0900

    add: [front] 스토리 화면 레이아웃 작업 중

---
+++ front/blogFront/src/components/story/List.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import Pagination from "../Pagination.svelte";
+import { link } from "svelte-spa-router";
+
+let page: number = 1;
+let count: number = 10;
+
+const TOTAl: number = 100;
+
+function setPagination(e: any) {
+  // 페이지 이동 버튼 클릭 시 이벤트
+  page = e.detail + 1;
+}
+</script>
+
+<ul class="sub-story-list">
+  {#each Array(5) as _, idx}
+    <li class="sub-story-item">
+      <a href="/story/{idx}" use:link>
+        <div class="sub-story-content-img">
+          <img
+            src="https://ridicorp.com/wp-content/uploads/2022/06/sf_1_thumb@0.5x.jpg"
+            alt="sample" />
+        </div>
+        <div class="sub-story-content-text">
+          <span>Backend</span>
+          <h2>제목</h2>
+          <p>devblog를 만들게 된 계기~</p>
+        </div>
+      </a>
+    </li>
+  {/each}
+</ul>
+
+<Pagination
+  bind:page
+  count="{TOTAl}"
+  bind:pageSize="{count}"
+  on:change="{setPagination}" />
--- front/blogFront/src/page/story/Story.svelte
+++ front/blogFront/src/page/story/Story.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 import Footer from "../../components/Footer.svelte";
 import Header from "../../components/Header.svelte";
-
-
+import List from "../../components/story/List.svelte";
 </script>
 
 <Header />
-<main>
- Stroy
+<main class="sub-page">
+  <section class="sub-story-wrap">
+    <div class="inner">
+      <div class="sub-story">
+        <h1 class="sub-page-title">Story</h1>
+        <div class="sub-story-contents">
+          <List />
+        </div>
+        <a href="#none">등록하기</a>
+      </div>
+    </div>
+  </section>
 </main>
 <Footer />

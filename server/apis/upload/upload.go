0c020ad783abf7776ffc227dffecb935f56ab15b
winter.kwon <winter.kwon@gbike.io>
2022-10-02 19:08:21 +0900

    s3 upload

--- server/apis/upload/upload.go
+++ server/apis/upload/upload.go
@@ -1,11 +1,11 @@
 package upload
 
 import (
-	"bytes"
 	"devLog/common/api_context"
 	"devLog/server/apis/context"
 	"devLog/server/s3"
 	"github.com/labstack/echo/v4"
+	"log"
 	"net/http"
 	"os"
 )
@@ -38,6 +38,9 @@ func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
 		return api_context.FailureJSON(http.StatusInternalServerError, "파일을 업로드에 실패하였습니다.")
 	}
 
+	log.Println("form data : ", form)
+	//return api_context.SuccessJSON(nil)
+
 	files := form.File["filename"]
 	if len(files) == 0 {
 		return api_context.NotFoundJSON("첨부된 파일이 없습니다.")
@@ -45,34 +48,42 @@ func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
 
 	file := files[0]
 
+	//fmt.Println("file : ", file)
+	//fmt.Println("FileName : ", app.req.FileName)
+	//fmt.Println("DirName : ", app.req.DirName)
+
 	f, err := file.Open()
 	if err != nil {
 		app.Log.WithError(err).Error("FileUpload src file open failure")
 		return api_context.InternalServerErrorJSON("파일을 여는데 실패하였습니다.")
 	}
 	defer f.Close()
-	fileInfo, _ := f.(*os.File).Stat()
+
+	fileInfo, err := f.(*os.File).Stat()
 	size := fileInfo.Size()
 	buffer := make([]byte, size)
 	f.Read(buffer)
-	fileBytes := bytes.NewReader(buffer)
+	//fileBytes := bytes.NewReader(buffer)
 
 	if err != nil {
 		app.Log.Error("cannot open file")
 		return api_context.FailureJSON(http.StatusInternalServerError, "file open failed.")
 	}
 
-	result, err := s3Manager.UploadFile(app.req.DirName, app.req.FileName, "image/jpeg", fileBytes, false)
-	if err != nil {
-		app.Log.Error("cannot upload file to s3 : ", err)
-		return api_context.FailureJSON(http.StatusInternalServerError, "파일 업로드 실패")
-	}
+	return api_context.SuccessJSON(nil)
 
-	resp := ImageUploadResponse{
-		FileUrl: result,
-	}
+	/*
+		result, err := s3Manager.UploadFile(app.req.DirName, app.req.FileName, "image/jpeg", fileBytes, false)
+		if err != nil {
+			app.Log.Error("cannot upload file to s3 : ", err)
+			return api_context.FailureJSON(http.StatusInternalServerError, "파일 업로드 실패")
+		}
 
-	return api_context.SuccessJSON(&resp)
+		resp := ImageUploadResponse{
+			FileUrl: result,
+		}
+
+		return api_context.SuccessJSON(&resp)*/
 }
 
 func (app *ServiceImageUpload) GetRequestData() interface{} {
@@ -84,7 +95,7 @@ func (app ServiceImageUpload) ApiName() interface{} {
 }
 
 func (app ServiceImageUpload) IsRequiredAuth() bool {
-	return true
+	return false
 }
 
 func ProcessImageUpload(c echo.Context) error {

53fa1766cb57862b92a1a7f3806d3a1a3bc19e26
winter.kwon <winter.kwon@gbike.io>
2022-10-08 22:47:57 +0900

    profile image updated

--- server/apis/upload/upload.go
+++ server/apis/upload/upload.go
@@ -4,14 +4,16 @@ import (
 	"devLog/common/api_context"
 	"devLog/server/apis/context"
 	"devLog/server/s3"
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"io/ioutil"
 	"net/http"
 )
 
-type ImageUploadRequest struct {
-	FileName string `form:"pic_name"`
-	DirName  string `form:"dir_name"`
+type ImageUpload struct {
+	FileName string `json:"pic_name"`
+	DirName  string `json:"dir_name"`
 }
 
 type ImageUploadResponse struct {
@@ -20,7 +22,6 @@ type ImageUploadResponse struct {
 
 type ServiceImageUpload struct {
 	*context.Context
-	req ImageUploadRequest
 }
 
 func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
@@ -37,15 +38,14 @@ func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
 	}
 
 	files := form.File["filename"]
+	fileInfos := form.File["file_info"]
 
 	if len(files) == 0 {
 		return api_context.NotFoundJSON("첨부된 파일이 없습니다.")
 	}
 
 	file := files[0]
-
-	fmt.Println("FileName : ", app.req.FileName)
-	fmt.Println("DirName : ", app.req.DirName)
+	fileInfo := fileInfos[0]
 
 	f, err := file.Open()
 	if err != nil {
@@ -59,7 +59,36 @@ func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
 		return api_context.FailureJSON(http.StatusInternalServerError, "file open failed.")
 	}
 
-	result, err := s3Manager.UploadFile(app.req.DirName, app.req.FileName, "image/jpeg", f, false)
+	f2, err := fileInfo.Open()
+	if err != nil {
+		app.Log.WithError(err).Error("FileUpload src file open failure")
+		return api_context.InternalServerErrorJSON("파일을 여는데 실패하였습니다.")
+	}
+	defer f2.Close()
+
+	if err != nil {
+		app.Log.Error("cannot open file")
+		return api_context.FailureJSON(http.StatusInternalServerError, "file open failed.")
+	}
+
+	bytes, err := ioutil.ReadAll(f2)
+	if err != nil {
+		app.Log.Error("cannot read file")
+		return api_context.FailureJSON(http.StatusInternalServerError, "file open failed.")
+	}
+
+	fmt.Println("read all : ", string(bytes))
+
+	imageInfo := ImageUpload{}
+	err = json.Unmarshal(bytes, &imageInfo)
+	if err != nil {
+		app.Log.Error("cannot unmarshal images")
+		return api_context.FailureJSON(http.StatusInternalServerError, "file open failed.")
+	}
+
+	fmt.Println("imageInfo : ", imageInfo)
+
+	result, err := s3Manager.UploadFile(imageInfo.DirName, imageInfo.FileName, "image/jpeg", f, true)
 	if err != nil {
 		app.Log.Error("cannot upload file to s3 : ", err)
 		return api_context.FailureJSON(http.StatusInternalServerError, "파일 업로드 실패")
@@ -75,7 +104,7 @@ func (app *ServiceImageUpload) Service() *api_context.CommonResponse {
 }
 
 func (app *ServiceImageUpload) GetRequestData() interface{} {
-	return &app.req
+	return nil
 }
 
 func (app ServiceImageUpload) ApiName() interface{} {
